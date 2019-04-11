b2a8b2717a74155029fe4b569afbebc939130414
Christopher Morrrison <morriscb@uw.edu>
2019-04-11 16:51:53 -0700

    Add pandas return.

--- python/lsst/dax/ppdb/ppdb.py
+++ python/lsst/dax/ppdb/ppdb.py
@@ -30,6 +30,7 @@ from datetime import datetime
 import logging
 import numpy as np
 import os
+import pandas
 
 import lsst.geom as geom
 import lsst.afw.table as afwTable
@@ -412,6 +413,81 @@ class Ppdb(object):
             with self._engine.begin() as conn:
                 res = conn.execute(query)
                 objects = self._convertResult(res, "DiaObject")
+        _LOG.debug("found %s DiaObjects", len(objects))
+        return objects
+
+    def getDiaObjectsPandas(self, pixel_ranges):
+        """Returns catalog of DiaObject instances from given region.
+
+        Objects are searched based on pixelization index and region is
+        determined by the set of indices. There is no assumption on a
+        particular type of index, client is responsible for consistency
+        when calculating pixelization indices.
+
+        This method returns :doc:`/modules/lsst.afw.table/index` catalog with schema determined by
+        the schema of PPDB table. Re-mapping of the column names is done for
+        some columns (based on column map passed to constructor) but types
+        or units are not changed.
+
+        Returns only the last version of each DiaObject.
+
+        Parameters
+        ----------
+        pixel_ranges : `list` of `tuple`
+            Sequence of ranges, range is a tuple (minPixelID, maxPixelID).
+            This defines set of pixel indices to be included in result.
+
+        Returns
+        -------
+        catalog : `lsst.afw.table.SourceCatalog`
+            Catalog contaning DiaObject records.
+        """
+
+        # decide what columns we need
+        if self.config.dia_object_index == 'last_object_table':
+            table = self._schema.objects_last
+        else:
+            table = self._schema.objects
+        if not self.config.dia_object_columns:
+            query = table.select()
+        else:
+            columns = [table.c[col] for col in self.config.dia_object_columns]
+            query = sql.select(columns)
+
+        if self.config.diaobject_index_hint:
+            val = self.config.diaobject_index_hint
+            query = query.with_hint(table, 'index_rs_asc(%(name)s "{}")'.format(val))
+        if self.config.dynamic_sampling_hint > 0:
+            val = self.config.dynamic_sampling_hint
+            query = query.with_hint(table, 'dynamic_sampling(%(name)s {})'.format(val))
+        if self.config.cardinality_hint > 0:
+            val = self.config.cardinality_hint
+            query = query.with_hint(table, 'FIRST_ROWS_1 cardinality(%(name)s {})'.format(val))
+
+        # build selection
+        exprlist = []
+        for low, upper in pixel_ranges:
+            upper -= 1
+            if low == upper:
+                exprlist.append(table.c.pixelId == low)
+            else:
+                exprlist.append(sql.expression.between(table.c.pixelId, low, upper))
+        query = query.where(sql.expression.or_(*exprlist))
+
+        # select latest version of objects
+        if self.config.dia_object_index != 'last_object_table':
+            query = query.where(table.c.validityEnd == None)  # noqa: E711
+
+        _LOG.debug("query: %s", query)
+
+        if self.config.explain:
+            # run the same query with explain
+            self._explain(query, self._engine)
+
+        # execute select
+        with Timer('DiaObject select', self.config.timer):
+            with self._engine.begin() as conn:
+                objects = pandas.read_sql_query(query, conn)
         _LOG.debug("found %s DiaObjects", len(objects))
         return objects
 
